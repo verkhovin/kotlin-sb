71479ac4f9b9d3c9951981730e0bc53ed5570b5a
Nikita Verkhovin <nikita.verkhovin@oneio.cloud>
2022-11-15 20:32:12 +0200

    Initial commit

---
+++ src/main/kotlin/SomeClass.kt
@@ -0,0 +1,5 @@
+class SomeClass {
+    fun aaa(): String {
+        return "AAAAAAAAAA"
+    }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,6 @@
+import java.lang.StringBuilder
+
+fun main(args: Array<String>) {
+    val s: String? = StringBuilder().toString()
+    val b = s?.toBoolean()
+}
